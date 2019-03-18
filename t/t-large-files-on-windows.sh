232b2a5705dab851bf58c784d51f9935049ec253
Philip Oakley <philipoakley@iee.org>
2019-03-18 18:56:39 +0000

    t-large-files-on-windows.sh: use gc to ensure packing
    when testing en-small, the loose object limit is not triggered,
    explicitly garbage collect (gc) to pack the repo.
    
    Signed-off-by: Philip Oakley <philipoakley@iee.org>

--- t/t-large-files-on-windows.sh
+++ t/t-large-files-on-windows.sh
@@ -6,13 +6,15 @@ test_description='test large file handling on windows'
 test_expect_success SIZE_T_IS_64BIT 'blah blubb' '
 
 	test-tool zlib-compile-flags >zlibFlags.txt &&
-	dd if=/dev/zero of=file bs=1M count=4 &&
+	dd if=/dev/zero of=file bs=1M count=5100 &&
 	git config core.compression 0 &&
 	git config core.looseCompression 0 &&
 	git add file &&
 	git fsck --verbose --strict --full &&
 	git commit -m msg file &&
 	git log --stat &&
+	git fsck --verbose --strict --full &&
+	git gc &&
 	git fsck --verbose --strict --full &&
 	git index-pack --verify .git/objects/pack/*.pack &&
 	git gc
